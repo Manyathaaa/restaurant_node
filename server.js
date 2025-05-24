3f2fbf971105dcf6e2d1df2dd71910dd25490892
Manyathaaa <mailtomanyatha@gmail.com>
2025-05-24 20:21:48 +0530

    extensions and packages installation

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from "express";
-
+import cors from "cors"; // cors is a middleware that allows cross-origin requests
+import morgan from "morgan"; // morgan is a middleware that logs HTTP requests
+import dotenv from "dotenv"; // dotenv is a module that loads environment variables from a .env file
 const app = express();
 
-const port = 1011;
+//configure dotenv
+dotenv.config();
+//middlewares
+app.use(morgan("dev")); // use morgan middleware to log requests in development mode...,, dev means development
+app.use(cors()); // use cors middleware
+app.use(express.json()); // use express json middleware to parse JSON requests
+
+const PORT = process.env.PORT;
 app.get("/", (req, res) => {
+  4;
   res.send("<h1>hello world</h1>");
 });
 
-app.listen(port, () => {
-  console.log("server is running in port " + port);
+app.listen(PORT, () => {
+  console.log("server is running in port " + PORT);
 });
