d5ff811546bf37b0e039c590c862d1df224a7001
Manyathaaa <mailtomanyatha@gmail.com>
2025-06-07 23:36:32 +0530

    get all food

--- controllers/foodcontrollers.js
+++ controllers/foodcontrollers.js
@@ -18,3 +18,28 @@ export const createfoodController = async (req, res) => {
     });
   }
 };
+
+//getall foods
+export const getallfoodController = async (req, res) => {
+  try {
+    const food = await foodModels.find({});
+    if (!food) {
+      return res.status(500).send({
+        success: false,
+        message: "provide food",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      totalfood: food.length,
+      food,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(404).send({
+      success: false,
+      message: "failed to get all food",
+      error,
+    });
+  }
+};
--- routes/foodroutes.js
+++ routes/foodroutes.js
@@ -1,10 +1,16 @@
 import express from "express";
 import authmiddleware from "../middlewares/authmiddleware.js";
-import { createfoodController } from "../controllers/foodcontrollers.js";
+import {
+  createfoodController,
+  getallfoodController,
+} from "../controllers/foodcontrollers.js";
 
 const router = express.Router();
 
 //create food
 router.post("/create", authmiddleware, createfoodController);
+
+//getall food
+router.get("/getall", authmiddleware, getallfoodController);
 
 export default router;
