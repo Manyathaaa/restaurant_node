d20ab8146f38fea8c87b3b960aade22428d18b6e
Manyathaaa <mailtomanyatha@gmail.com>
2025-06-07 16:07:03 +0530

    get all restuarant and single restaurant api nodes

--- controllers/restaurantcontroller.js
+++ controllers/restaurantcontroller.js
@@ -19,3 +19,61 @@ export const createRestaurantController = async (req, res) => {
     });
   }
 };
+
+//get all reataurants
+
+export const getAllrestaurantController = async (req, res) => {
+  try {
+    const restaurant = await restaurantModel.find({});
+    if (!restaurant) {
+      res.status(500).send({
+        success: false,
+        message: "no restaurant available",
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      totalCount: restaurant.length,
+      restaurant,
+    });
+  } catch (error) {
+    console.log("something is error", error);
+    return res.status(404).send({
+      success: false,
+      message: "failed to get all restaurants",
+      error,
+    });
+  }
+};
+
+//get restaurant by id
+export const getrestaurantByIdController = async (req, res) => {
+  try {
+    const restaurantId = req.params.id;
+    if (!restaurantId) {
+      return res.status(500).send({
+        success: false,
+        message: "provide restaurant id",
+      });
+    }
+    const restaurant = await restaurantModel.findById(restaurantId);
+    if (!restaurant) {
+      return res.status(500).send({
+        success: false,
+        message: "no restaurant found",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "successfully done",
+    });
+  } catch (error) {
+    console.log("something error", error);
+    return res.status(404).send({
+      success: false,
+      message: "failed to get restaurant by id",
+      error,
+    });
+  }
+};
--- routes/restaurantroutes.js
+++ routes/restaurantroutes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { createRestaurantController } from "../controllers/restaurantcontroller.js"; // Correct import
+import {
+  createRestaurantController,
+  getAllrestaurantController,
+  getrestaurantByIdController,
+} from "../controllers/restaurantcontroller.js"; // Correct import
 
 const router = express.Router();
 
 // Define the create restaurant route
 router.post("/create", createRestaurantController); // Use the correct controller
+
+//get all restaurant
+router.get("/getall", getAllrestaurantController);
+
+//get by id
+router.get("/get/:id", getrestaurantByIdController);
 
 export default router;
