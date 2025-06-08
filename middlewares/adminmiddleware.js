be248b314b58492434b281f22fb4b434c6802f70
Manyathaaa <mailtomanyatha@gmail.com>
2025-06-08 14:11:40 +0530

    adminmiddleware and order status done successfully

--- middlewares/adminmiddleware.js
+++ middlewares/adminmiddleware.js
@@ -2,7 +2,7 @@ import userModel from "../models/usersModel.js"; // Correct import path
 
 export const adminmiddleware = async (req, res, next) => {
   try {
-    const user = await userModel.findById(req.body.id); // Correct reference to userModel
+    const user = await userModel.findById(req.user.id); // Correct reference to userModel
     if (!user || user.usertype !== "admin") {
       return res.status(403).send({
         success: false,
